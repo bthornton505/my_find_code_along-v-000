4b8f26f37210587170089dd6ab349b1baefd6a1d
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-17 12:44:55 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-      return collection[i] if yield(collection[i])
+      return collection[i] if yield(collection[i])  # this has been refactored from classic if statement 
     i += 1 
   end 
 end
